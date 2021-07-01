05902ce1a1be6798fccc338f678c27271413552f
Cedgo <cedgo1997@gmail.com>
2021-07-01 02:38:38 -0400

    Showing images

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,16 +1,19 @@
-import React, {useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
+import { GifGridItem } from './GifGridItem';
 
-export const GifGrid = ({category}) => {
+export const GifGrid = ({ category }) => {
 
-    useEffect( () => {
+    useEffect(() => {
         getGifs();
     }, []);
+
+    const [images, setImages] = useState([]);
 
     const getGifs = async () => {
         const apiKey = 'EfiDDliYy1sKOtC0ePfLVayOWsFVNM9x';
         const url = `https://api.giphy.com/v1/gifs/search?q=rick&limit=10&api_key=${apiKey}`;
         const resp = await fetch(url);
-        const {data} = await resp.json();
+        const { data } = await resp.json();
         const gifs = data.map(img => {
             return {
                 id: img.id,
@@ -18,12 +21,19 @@ export const GifGrid = ({category}) => {
                 url: img.images?.downsized_medium.url
             }
         });
-        console.log(gifs)
+        setImages(gifs);
     }
 
     return (
         <div>
             <h3>{category}</h3>
+            <ol>
+                {images.map((img) => 
+                        <GifGridItem
+                            img = {img}
+                            {...img}
+                        />)}
+            </ol>
         </div>
     )
 }
