9a3a41d01ca357c374e125c0bc0a2a51ec520b00
Cedgo <cedgo1997@gmail.com>
2021-07-01 01:28:00 -0400

    Form created, preparing to communicate components

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,24 +1,24 @@
 import React, { useState } from 'react';
 import { Fragment } from 'react';
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
 
-    /* const categories = ['One Punch', 'Samurai X', 'Dragon Ball']; */
 
     const initialState = ['One Punch', 'Samurai X', 'Dragon Ball'];
     const [categories, setCategories] = useState(initialState);
 
-    const handleAdd = () => {
+/*     const handleAdd = () => {
         const category = 'Pokemon';
         setCategories((state) => [...state, category]);
-    }
+    }; */
 
     return (
         <Fragment>
             <h2>GifExpertApp</h2>
+            <AddCategory />
             <hr />
 
-            <button onClick={handleAdd}>Add New</button>
             <ol>
                 {
                     categories.map((item) => <li key={item}>{item}</li>)
