8957a5d298d7c818e30f0764c1fec193f631b5c7
Cedgo <cedgo1997@gmail.com>
2021-07-01 00:19:05 -0400

    feat: GifExpertApp component created

---
+++ src/GifExpertApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Fragment } from 'react';
+
+export const GifExpertApp = () => {
+    return (
+        <Fragment>
+            <h2>GifExpertApp</h2>
+            <hr />
+        </Fragment>
+    )
+}
