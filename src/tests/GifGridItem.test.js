698b4ae1f65e2232c18db7821e4fe8438fae1848
Cedgo <cedgo1997@gmail.com>
2021-07-02 16:30:07 -0400

    Test en GifGridItem hechos

--- src/tests/GifGridItem.test.js
+++ src/tests/GifGridItem.test.js
@@ -1,15 +1,35 @@
 import { shallow } from "enzyme";
 import { GifGridItem } from "../components/GifGridItem";
+import React from 'react';
 
 describe('Pruebas en GifGridItem', () => {
-    test('El componente se debe renderizar correctamente', () =>{
 
-        const image = {
-            id: 1,
-            title: 'Imagen',
-            url: 'https://media.giphy.com/media/26gsad5RsZVhKsUDe/giphy.gif'
-        }
-        const wrapper = shallow(<GifGridItem {...image} />);
+    const image = {
+        id: 1,
+        title: 'Imagen',
+        url: 'https://media.giphy.com/media/26gsad5RsZVhKsUDe/giphy.gif'
+    }
+    let wrapper = shallow(<GifGridItem {...image} />);
+    beforeEach(() => {
+        wrapper = shallow(<GifGridItem {...image} />);
+    });
+
+    test('El componente se debe renderizar correctamente', () => {
         expect(wrapper).toMatchSnapshot();
-    })
+    });
+
+    test('Debe tener un párrafo con el título', () => {
+        const textoParrafo = wrapper.find('p').text().trim();
+        expect(textoParrafo).toBe(image.title);
+    });
+    test('Debe tener la imagen igual al url y alt de los props', () => {
+        const img = wrapper.find('img');
+        expect(img.prop('src')).toBe(image.url);
+        expect(img.prop('alt')).toBe(image.title);
+
+    });
+    test('Debe div tener className animated__fadeIn', () => {
+        const container = wrapper.find('div').hasClass('card');
+        expect(container).toBeTruthy();
+    });
 });
